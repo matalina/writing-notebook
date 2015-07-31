d5fc4e1ec01c04f265facb1bf8fb693452ffbd51
Alicia Wilkerson <aliciawilkerson@babers.com>
2015-07-31 08:58:14 -0500

    set up elixir, fondation and split markdown screen

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,21 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.sass([
+        'app.scss',
+        '../js/crevasse/dependencies/solarized-light.css',
+        '../js/crevasse/lib/css/crevasse.css'
+    ]);
+
+    mix.scripts([
+        '../bower_components/jquery/dist/jquery.js',
+        '../bower_components/foundation/js/vendor/fastclick.js',
+        '../bower_components/foundation/foundation.min.js',
+        'crevasse/dependencies/jquery.caret.js',
+        'crevasse/dependencies/jquery.scrollTo.min.js',
+        'crevasse/dependencies/marked.js',
+        'crevasse/dependencies/rainbow-custom.min.js',
+        'crevasse/lib/js/crevasse.js',
+        'app.js'
+    ]);
 });

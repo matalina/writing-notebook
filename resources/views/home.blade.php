e57e17f33cdfb5bfa2f8f71932bbc327dbb9249f
Alicia Wilkerson <aliciawilkerson@babers.com>
2015-08-03 15:03:23 -0500

    working on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,60 +13,60 @@
         </div>
         <div class="row">
             <div class="small-12 medium-6">
-                <ul class="example-orbit" data-orbit>
-                    <li class="active">
-                        <p>
-                            “you can, you should, and if you’re brave enough to start, you will.”
-                            ? Stephen King, On Writing: A Memoir of the Craft
+                <ul class="quote-orbit" data-orbit>
+                    <li  class="active">
+                        <p  class="orbit-caption">
+                            "you can, you should, and if you’re brave enough to start, you will."
+                            - Stephen King, On Writing: A Memoir of the Craft
                         </p>
                     </li>
-                    <li class="active">
-                        <p>
-                            “Imagination is like a muscle. I found out that the more I wrote, the bigger it got.”
-                            ? Philip José Farmer
+                    <li>
+                        <p  class="orbit-caption">
+                            "Imagination is like a muscle. I found out that the more I wrote, the bigger it got."
+                            - Philip José Farmer
                         </p>
                     </li>
-                    <li class="active">
-                        <p>
-                            “Fantasy is hardly an escape from reality. It's a way of understanding it.”
-                            ? Lloyd Alexander
+                    <li>
+                        <p  class="orbit-caption">
+                            "Fantasy is hardly an escape from reality. It's a way of understanding it."
+                            - Lloyd Alexander
                         </p>
                     </li>
-                    <li class="active">
-                        <p>
-                            “You can make anything by writing.”
-                            ? C.S. Lewis
+                    <li>
+                        <p  class="orbit-caption">
+                            "You can make anything by writing."
+                            - C.S. Lewis
                         </p>
                     </li>
-                    <li class="active">
-                        <p>
-                            “There is nothing to writing. All you do is sit down at a typewriter and bleed.”
-                            ? Ernest Hemingway
+                    <li>
+                        <p  class="orbit-caption">
+                            "There is nothing to writing. All you do is sit down at a typewriter and bleed."
+                            - Ernest Hemingway
                         </p>
                     </li>
-                    <li class="active">
-                        <p>
-                            “Writing is a socially acceptable form of schizophrenia.”
-                            ? E.L. Doctorow
+                    <li>
+                        <p  class="orbit-caption">
+                            "Writing is a socially acceptable form of schizophrenia."
+                            - E.L. Doctorow
                         </p>
                     </li>
-                    <li class="active">
-                        <p>
-                            “Start writing, no matter what. The water does not flow until the faucet is turned on.”
-                            ? Louis L'Amour
+                    <li>
+                        <p  class="orbit-caption">
+                            "Start writing, no matter what. The water does not flow until the faucet is turned on."
+                            - Louis L'Amour
                         </p>
                     </li>
-                    <li class="active">
-                        <p>
-                            “Being a writer is a very peculiar sort of a job: it's always you versus a blank sheet of
-                            paper (or a blank screen) and quite often the blank piece of paper wins.”
-                            ? Neil Gaiman
+                    <li>
+                        <p  class="orbit-caption">
+                            "Being a writer is a very peculiar sort of a job: it's always you versus a blank sheet of
+                            paper (or a blank screen) and quite often the blank piece of paper wins."
+                            - Neil Gaiman
                         </p>
                     </li>
-                    <li class="active">
-                        <p>
-                            “Ordinary life is pretty complex stuff.”
-                            ? Harvey Pekar
+                    <li>
+                        <p  class="orbit-caption">
+                            "Ordinary life is pretty complex stuff."
+                            - Harvey Pekar
                         </p>
                     </li>
 
